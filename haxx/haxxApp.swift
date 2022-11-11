91acaa4c17538bf91cb9d66976ddebf85bd51748
alfiecg24 <alfiecockell@outlook.com>
2022-11-11 17:01:49 +0000

    Initial Commit

---
+++ haxx/haxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  haxxApp.swift
+//  haxx
+//
+//  Created by Alfie on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct haxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
