118cb61a54bf94e690d16467ffb96346f2acd9a6
alfiecg24 <alfiecockell@outlook.com>
2022-11-11 18:42:41 +0000

    Hopefully works

---
+++ haxx/haxx-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  haxx
+//
+//  Created by Alfie on 11/11/2022.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+
+#import "exploit/exploit.h"
+
+int exploit(void);
--- haxx/haxxApp.swift
+++ haxx/haxxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct haxxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ExploitView()
         }
     }
 }
